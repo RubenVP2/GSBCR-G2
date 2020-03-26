c3c1a90fc58f849246b4a999a531944c30df38ba
JolanLar <jolanlargeteau@gmail.com>
2020-03-26 15:10:48 +0100

    push

--- client/src/app/medecin/medecin.component.ts
+++ client/src/app/medecin/medecin.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medecin.component.css']
 })
 export class MedecinComponent implements OnInit {
+  title = 'Medecin';
 
   constructor() { }
 
