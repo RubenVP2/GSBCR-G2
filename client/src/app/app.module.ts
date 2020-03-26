90bf5b6a91554ff6ad0c46497254cfbcbc67392e
KagayaTV <victorravaz.pro@gmail.com>
2020-03-26 14:41:21 +0100

    component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,11 +9,17 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { AuthService } from './services/auth.service';
 import { ErreurComponent } from './erreur/erreur.component';
 import {AuthGuard} from './services/auth-guard.service';
+import { VisiteComponent } from './visite/visite.component';
+import { MedecinComponent } from './medecin/medecin.component';
+import { MedicamentComponent } from './medicament/medicament.component';
 
 // Les Routes
 const appRoutes: Routes = [
   { path: 'connexion', component: AuthComponent},
   { path: 'home', canActivate: [AuthGuard], component: AccueilComponent},
+  { path: 'visite', canActivate: [AuthGuard], component: VisiteComponent},
+  { path: 'medecin', canActivate: [AuthGuard], component: MedecinComponent},
+  { path: 'medicament', canActivate: [AuthGuard], component: MedicamentComponent},
   { path: '', component: AuthComponent},
   { path: 'not-found', component: ErreurComponent},
   { path: '**', redirectTo: '/not-found'},
@@ -24,7 +30,10 @@ const appRoutes: Routes = [
     AppComponent,
     AuthComponent,
     AccueilComponent,
-    ErreurComponent
+    ErreurComponent,
+    VisiteComponent,
+    MedecinComponent,
+    MedicamentComponent
   ],
   imports: [
     BrowserModule,
